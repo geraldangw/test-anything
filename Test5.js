9621c473c4ed5b9905bf9c43d7e00bbe27ac44b2
Gerald Ang <geraldangw@gmail.com>
2016-08-10 10:30:18 +0800

    revised answers

--- Test5.js
+++ Test5.js
@@ -1,7 +1,8 @@
 var feedCat = require(process.argv[2]);
 var test = require('tape');
 test('feedCat', function (t) {
-	t.equal(feedCat('food'), 'yum', 'feedCat food should be yum');
-	t.throws(feedCat.bind(null, 'chocolate'), null, 'feedCat chocolate should throw error');
+  t.plan(2);
+	t.equal(feedCat('food'), 'yum');
+	t.throws(feedCat.bind(null, 'chocolate'), null);
 	t.end();
 });
